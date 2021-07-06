25b1cb5a3f92cb14d1b8859d6f4ac2ac6b20a07d
moorthyappinessworld <86701421+moorthyappinessworld@users.noreply.github.com>
2021-07-06 16:54:52 +0530

    Add files via upload

--- operation.js
+++ operation.js
@@ -1,61 +1,74 @@
-const optionValue = "";
-        function viewOptions() {
-            var e = document.getElementById("service");
-            optionValue = e.options[e.selectedIndex].text;
+/*
+    After clicking the "Calculate" Button the showBill() function 
+    will processed. It takes input like Type of service, Amount and 
+    How many people are sharing the Total Amount. It generates the 
+    output like Tip Amount, Total Amount and Amount Share by person.
+ */
+function showBill() {
+    const service = document.getElementById("service");
+    const optionValue = service.selectedIndex;
+    const amountCheck = document.getElementById("amount").value;
+    const countCheck = document.getElementById("count").value;
+    const symbol = '+';
+    if (amountCheck === "") {
+        alert("Please Enter the Amount");
+        return
+    } else if (!optionValue) {
+        alert("Please select the Service Options");
+        return
+    } else if (countCheck === "") {
+        alert("Please Enter the 'How many people are sharing the Bill?'");
+        return
+    } else {
+        document.getElementById("showOutput").style.display = "block";
+        if (optionValue === 1) {
+            tipTotal = eval(amountCheck * 0.1);
+        } else if (optionValue === 2) {
+            tipTotal = eval(amountCheck * 0.08);
+        } else if (optionValue === 3) {
+            tipTotal = eval(amountCheck * 0.06);
+        } else if (optionValue === 4) {
+            tipTotal = eval(amountCheck * 0.04);
+        } else if (optionValue === 5) {
+            tipTotal = eval(amountCheck * 0.02);
         }
-        function showOutput() {
-            const amountCheck = document.getElementById("amount").value;
-            const countCheck = document.getElementById("count").value;
-            const symbol = '+';
-            if (!optionValue) {
-                document.getElementById('tip').innerHTML = "Please select the Options";
-                document.getElementById('total').innerHTML = "Please select the Options";
-                document.getElementById('share').innerHTML = "Please select the Options";
-            } else if (amountCheck === "") {
-                document.getElementById('tip').innerHTML = "Please Enter the Aount";
-                document.getElementById('total').innerHTML = "Please Enter the Aount";
-                document.getElementById('share').innerHTML = "Please Enter the Aount";
-            } else if (countCheck === "") {
-                document.getElementById('tip').innerHTML = "Please Enter the Count";
-                document.getElementById('total').innerHTML = "Please Enter the Count";
-                document.getElementById('share').innerHTML = "Please Enter the Count";
-            } else {
-                if (optionValue === "Outstanding") {
-                    const tipTotal = eval(amountCheck * 0.10);
-                    const Total = eval(amountCheck + symbol + tipTotal);
-                    const Sharing = eval(Total / countCheck);
-                    document.getElementById('tip').innerHTML = tipTotal;
-                    document.getElementById('total').innerHTML = Total;
-                    document.getElementById('share').innerHTML = Sharing;
-                } else if (optionValue === "Good") {
-                    const tipTotal = eval(amountCheck * 0.08);
-                    const Total = eval(amountCheck + symbol + tipTotal);
-                    const Sharing = eval(Total / countCheck);
-                    document.getElementById('tip').innerHTML = tipTotal;
-                    document.getElementById('total').innerHTML = Total;
-                    document.getElementById('share').innerHTML = Sharing;
-                } else if (optionValue === "Okay") {
-                    const tipTotal = eval(amountCheck * 0.06);
-                    const Total = eval(amountCheck + symbol + tipTotal);
-                    const Sharing = eval(Total / countCheck);
-                    document.getElementById('tip').innerHTML = tipTotal;
-                    document.getElementById('total').innerHTML = Total;
-                    document.getElementById('share').innerHTML = Sharing;
-                } else if (optionValue === "Bad") {
-                    const tipTotal = eval(amountCheck * 0.04);
-                    const Total = eval(amountCheck + symbol + tipTotal);
-                    const Sharing = eval(Total / countCheck);
-                    document.getElementById('tip').innerHTML = tipTotal;
-                    document.getElementById('total').innerHTML = Total;
-                    document.getElementById('share').innerHTML = Sharing;
-                } else {
-                    const tipTotal = eval(amountCheck * 0.02);
-                    const Total = eval(amountCheck + symbol + tipTotal);
-                    const Sharing = eval(Total / countCheck);
-                    document.getElementById('tip').innerHTML = tipTotal;
-                    document.getElementById('total').innerHTML = Total;
-                    document.getElementById('share').innerHTML = Sharing;
-                }
-            }
-
-        }
+        const Total = eval(amountCheck + symbol + tipTotal);
+        const Sharing = eval(Total / countCheck);
+        document.getElementById('tip').innerHTML = tipTotal;
+        document.getElementById('total').innerHTML = Total;
+        document.getElementById('share').innerHTML = Sharing;
+    }
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (p) {
+            const LatLng = new google.maps.LatLng(p.coords.latitude, p.coords.longitude);
+            const mapOptions = {
+                center: LatLng,
+                zoom: 10,
+                mapTypeId: google.maps.MapTypeId.ROAD_MAP
+            };
+            const geocoder = new google.maps.Geocoder();
+            const map = new google.maps.Map(document.getElementById("displayMap"), mapOptions);
+            geocoder.geocode({ location: LatLng })
+                .then((response) => {
+                    if (response.results[0]) {
+                        map.setZoom(10);
+                        const marker = new google.maps.Marker({
+                            position: LatLng,
+                            map: map,
+                            title: "<div style = 'height:160px;width:160px'><b>Location:</b><br />" + response.results[0].formatted_address
+                        });
+                        google.maps.event.addListener(marker, "click", function (e) {
+                            const infoWindow = new google.maps.InfoWindow();
+                            infoWindow.setContent(marker.title);
+                            infoWindow.open(map, marker);
+                        });
+                    } else {
+                        window.alert("No results found");
+                    }
+                })
+                .catch((e) => window.alert("Geocoder failed due to: " + e));
+        });
+    } else {
+        alert('Geo Location feature is not supported in this browser.');
+    }
+}
